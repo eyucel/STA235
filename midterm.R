753821833a114d734cd877a5cc1e66c02e62f4cb
eyucel <eyucel@users.noreply.github.com>
2021-10-21 20:19:05 -0500

    update income variable

--- midterm.R
+++ midterm.R
@@ -12,7 +12,7 @@ library(ggfortify)
 library(ISLR)
 
 data(state)
-state <- data.frame(state.x77 ) %>% rownames_to_column("state") %>% mutate(Income = Income/10000)
+state <- data.frame(state.x77 ) %>% rownames_to_column("state") %>% mutate(Income = Income/1000)
 rm(state.x77, state.abb, state.area, state.division, state.name, state.region, state.center)
 
 diamonds <- ggplot2::diamonds
